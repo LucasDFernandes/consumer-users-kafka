879709270c1c4a940e2168e27748e46fba58cebf
lucasfernandes <lucasfernandes@INOTE702>
2020-02-11 17:04:35 -0300

    initial commit

---
+++ service-users-kafka/src/main/java/br/com/alura/users/kafka/ServiceUsersKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.users.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUsersKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceUsersKafkaApplication.class, args);
+	}
+
+}
